847dc66343cd07a38579209ae5eaf07cb2fa6e3e
JMollinedo <jmollinedoa@outlook.es>
2018-02-23 23:35:44 -0600

    Carga 01

---
+++ Lab02_ED1/Lab02_ED1/DataBase/DataAdmin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab02_ED1.DataBase
+{
+    public class DataAdmin
+    {
+        private static volatile DataAdmin Instance;
+        private static object syncRoot = new object();
+
+        private DataAdmin()
+        {
+
+        }
+
+        public static DataAdmin getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DataAdmin();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+    }
+}
--- Lab02_ED1/Lab02_ED1/Models/Country.cs
+++ Lab02_ED1/Lab02_ED1/Models/Country.cs
@@ -6,21 +6,70 @@ using System.Web;
 
 namespace Lab02_ED1.Models
 {
-    public class Country 
+    public class Country : IComparable
     {
         /*[Key]
-        public int Id { get; set; }*/
-
+         *public int Id { get; set; }
+         */
+        
+        /// <summary>
+        /// Nombre de Pais
+        /// </summary>
+        [Key]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Campo Requerido")]
         [Display(Name = "Nombre")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Grupo en Campeonato
+        /// </summary>
         [Required(AllowEmptyStrings = false, ErrorMessage = "Campo Requerido")]
         [Display(Name = "Grupo")]
         public char Group { get; set; }
 
+        /// <summary>
+        /// Comparador por Nombre
+        /// </summary>
+        /// <param name="country">Pais para comparar</param>
+        /// <returns>Valor de Comparacion</returns>
+        public int CompareByName(Country country)
+        {
+            return country.Name.CompareTo(Name);
+        }
 
+        /// <summary>
+        /// Comparador por Grupo
+        /// </summary>
+        /// <param name="country">Pais para comparar</param>
+        /// <returns>Valor de Comparacion</returns>
+        public int CompareByGroup(Country country)
+        {
+            return country.Group.CompareTo(Group);
+        }
 
-        
+        /// <summary>
+        /// Delegado de Comparadores
+        /// </summary>
+        /// <param name="country">Pais para comparar</param>
+        /// <returns>Valor de Comparacion</returns>
+        public delegate int Comparers(Country country);
+
+        /// <summary>
+        /// Comparador con Objeto
+        /// </summary>
+        /// <param name="obj">Objeto</param>
+        /// <returns>Valor de Comparacion</returns>
+        public int CompareTo(object obj)
+        {
+            try
+            {
+                Country country = obj as Country;
+                return CompareByName(country);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
