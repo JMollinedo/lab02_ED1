0d52e3280cd1e4b3bec725df3e6326fd10050f1a
PDGL1006517 <pablo_082006@hotmail.cpm>
2018-02-24 15:23:15 -0600

    Remover estos cambios

---
+++ Lab02_ED1/Lab02_ED1/Controllers/CountryController.cs
@@ -0,0 +1,131 @@
+﻿using Lab02_ED1.DataBase;
+using Lab02_ED1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02_ED1.Controllers
+{
+    public class CountryController : Controller
+    {
+        // GET: Country
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Country/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Country/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Country/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Country/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Country/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Country/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Country/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (upload != null && upload.ContentLength > 0)
+                {
+
+                    if (upload.FileName.EndsWith(".csv"))
+                    {
+                        JsonReader<Country> LectorJson = new JsonReader<Country>();
+                        List<Country> CountryList = LectorJson.Datos(upload.FileName);
+
+
+
+                        return View();
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
+            return View();
+        }
+    }
+}
